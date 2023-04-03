d19a1a4fa987be8175ad54cba02690c0290e987b
Ch3n4v <124992595+nsk-chenav@users.noreply.github.com>
2023-04-03 17:19:01 +0200

    %i does not exist in java string format, use %o for octal

--- src/main/java/net/savantly/graphite/query/impl/UntilImpl.java
+++ src/main/java/net/savantly/graphite/query/impl/UntilImpl.java
@@ -14,7 +14,7 @@ public class UntilImpl implements Until {
 	}
 	
 	public UntilImpl(int value, GraphiteTimeUnit unit) {
-		this.value = String.format("%i%s", value, unit.value());
+		this.value = String.format("%o%s", value, unit.value());
 	}
 
 
